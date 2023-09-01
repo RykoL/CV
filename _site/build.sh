f29561470d3d909d48ee6bf1bab56fa8fd795500
Rico Lang <rico.lang@thoughtworks.com>
2023-09-01 20:35:29 +0200

    Remove _site folder from repository

--- _site/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-wkhtmltopdf --enable-local-file-access -L 0mm -B 0mm -R 0mm -T 0mm index.html cv.pdf
--- _site/default.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import (fetchTarball https://github.com/nixos/nixpkgs/archive/9d8a066fd49dbd026e9b6c02c71a7d221e47e84e.tar.gz) {} }:
-
-
-pkgs.mkShell {
-  buildInputs = [
-  ];
-}
